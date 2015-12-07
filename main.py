b27e6acf0fe47776a46cb0eb2124495868931ac9
Rebekah Aimee Yoder <rebekahaimeeyoder@gmail.com>
2015-12-06 20:14:59 -0600

    Left off here

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.listview import ListView
+from kivy.uix.listview import ListView, ListItemButton
 from kivy.factory import Factory
 from kivy.properties import ListProperty, StringProperty, ObjectProperty
-from kivy.uix.listview import ListItemButton
+from kivy.storage.jsonstore import JsonStore
 
 import json
 
-#version 0.0.7
+#version 0.0.8
 
 class TinypapersApp(App):
     pass
     
 class TinypapersWindowManager(BoxLayout):
-
+    add_entry_text = ObjectProperty()
+    # This is also declared under the Add page. Sort out the program's 
+    # structure before you program JSON in formally.
+    def __init__(self, **kwargs):
+        super(TinypapersWindowManager, self).__init__(**kwargs)
+        #self.store = JsonStore("window_mgr_store.json")
+        #if self.store.exists('entries'):
+        #    print "Store found!"
+            
+    def save_entry(self):
+        #working on this code
+        # Needs to access the add_entry_text through an ObjectProperty.
+        pass
+            
     def display_add_form(self):
         self.clear_widgets()
         self.add_widget(AddPage())
@@ -40,9 +53,11 @@ class IndexPage(BoxLayout):
 class AddPage(BoxLayout):
     add_entry_text = ObjectProperty()
 
-    def add_entry(self):
+    def add_entry(self): #also pass other entry fields: eg content, phone #
+        #maybe *args should handle parsing the arrays later on
+        #that would allow code reuse because you could also pass a type var
+        #and add a switch statement to unpack depending on what type
         IndexPage.button_list_entries.append(self.add_entry_text.text)
-        #updates an iteration too late...
 
 class ConfirmationPage(AnchorLayout):
     pass

8bad70ecb93979ecf33d136a927b8e754ed1bc75
Rebekah Aimee Yoder <rebekahaimeeyoder@gmail.com>
2015-12-06 20:10:15 -0600

    Strip out formatting for debugging

--- main.py
+++ main.py
@@ -4,24 +4,40 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.listview import ListView
 from kivy.factory import Factory
-from kivy.properties import ListProperty, StringProperty
+from kivy.properties import ListProperty, StringProperty, ObjectProperty
 from kivy.uix.listview import ListItemButton
 
-#version 0.0.4
+#version 0.0.5
 
 class TinypapersApp(App):
     pass
     
 class TinypapersWindowManager(BoxLayout):
-    pass
+
+    def display_add_form(self):
+        self.clear_widgets()
+        self.add_widget(AddPage())
+        
+    def display_main_page(self):
+        self.clear_widgets()
+        self.add_widget(MainPage())
     
 class BasicListPage(BoxLayout):
-    pass
-
+    button_list_entries = []
+    
 class MainPage(BasicListPage):
     pass
+
+class AddPage(BoxLayout):
+    add_entry_text = ObjectProperty()
+
+    def add_entry(self):
+        BasicListPage.button_list_entries.append(self.add_entry_text.text)
     
 class ButtonList(ListView):
+    pass
+
+class TinypapersHeader(BoxLayout):
     pass
 
 class ItemButton(ListItemButton):

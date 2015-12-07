da01a594747d4846d84cd91dc76e46a5e76ad5b6
Rebekah Aimee Yoder <rebekahaimeeyoder@gmail.com>
2015-12-06 20:11:59 -0600

    Refactor

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.listview import ListView
 from kivy.factory import Factory
 from kivy.properties import ListProperty, StringProperty, ObjectProperty
 from kivy.uix.listview import ListItemButton
 
-#version 0.0.5
+#version 0.0.6
 
 class TinypapersApp(App):
     pass
     
 class TinypapersWindowManager(BoxLayout):
+    # render MainPage only once and store it?
+    #main_page = ObjectProperty()
 
     def display_add_form(self):
         self.clear_widgets()
         self.add_widget(AddPage())
         
+    def display_index_page(self):
+        self.clear_widgets()
+        self.add_widget(IndexPage())
+            
     def display_main_page(self):
         self.clear_widgets()
         self.add_widget(MainPage())
-    
-class BasicListPage(BoxLayout):
-    button_list_entries = []
-    
-class MainPage(BasicListPage):
-    pass
+        
+        # figure out how to render MainPage only once and store it?
+        #if self.main_page is None:
+        #    self.main_page = self.add_widget(MainPage())
+        #if self.main_page is not None:
+        #    self.clear_widgets()
+        #    self.add_widget(self.main_page)
 
+class MainPage(BoxLayout):
+    pass
+    
+class IndexPage(BoxLayout):
+    button_list_entries = []
+    #entry_list = ObjectProperty()
+    
+    #def __init__(self, **kwargs):
+    #    self.update_entries()
+
+    #def update_entries(self):
+    #    self.entry_list.populate()
+        
 class AddPage(BoxLayout):
     add_entry_text = ObjectProperty()
 
     def add_entry(self):
-        BasicListPage.button_list_entries.append(self.add_entry_text.text)
-    
-class ButtonList(ListView):
-    pass
-
-class TinypapersHeader(BoxLayout):
-    pass
-
-class ItemButton(ListItemButton):
-    pass
+        IndexPage.button_list_entries.append(self.add_entry_text.text)
+        #updates an iteration too late...
 
 if __name__ == '__main__':
     TinypapersApp().run()
